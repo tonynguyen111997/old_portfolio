cea3237034a537db493349999e934556e04db9bd
tonynguyen111997 <tonynguyen111997@gmail.com>
2019-01-25 20:24:34 -0800

    Update Info
    Updated portfolio to include Ledkyb. Removed skills
    from about me and made it justified.

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -16,13 +16,12 @@ const AboutMe = ({aboutOpac}) => {
 				<h3>Tony Nguyen</h3>
 				<h4>tonynguyen111997@gmail.com</h4>
 				<h3>Web Developer</h3>
-				<p>Self taught web developer looking to further develop web development skills. 
-				I am a passionate learner that is eager to gain more experience.
-				Learning to program has been a passion of mine for many years now. A key aspect of 
-				programming that has kept me interested in it is the problem solving.
-				I enjoy problem solving, as it gives me a challenge and the opportunity to develop my 
-				critical thinking skills. Currently I have experience with HTML, CSS, Bootstrap, 
-				SASS/SCSS,  Javascript, React, Redux, Node.js, Express.js, Git and GitHub.</p>
+				<p>Self taught web developer that is always looking to grow and improve. 
+					I am a passionate learner that is eager to gain more experience. Learning 
+					to program has been a passion of mine for many years now. A key aspect 
+					of programming that has kept me interested in it is the problem solving. 
+					I enjoy problem solving, as it gives me a challenge and the opportunity to 
+					develop my critical thinking skills.</p>
 				<SocialNetworksButtons containerClass="about-me-btn-container"/>
 			</div>
 		</div>
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './Portfolio.scss';
 
 //portfolio imgs
+import ledkybImg from '../../public/portfolio/ledkyb.png';
 import fccImg from '../../public/portfolio/fcc_riverside.png';
 import smilurImg from '../../public/portfolio/smilur.png';
 import simonImg from '../../public/portfolio/simon_game.png';
@@ -18,16 +19,25 @@ const Portfolio = ({portfolioOpac}) => {
 		<div id="portfolio-container" className="portfolio">
 			<h2 id="portfolio" style={{opacity: portfolioOpac}}>Portfolio</h2>
 			<div className="card-container" style={{opacity: portfolioOpac}}>
-				<Card id="card1" img={fccImg} alt="free code camp riverside github" 
+				<Card id="card1" img={ledkybImg} alt="ledkyb website" 
+					link="https://www.ledkyb.com/"
+					smallDesc="Worked with the Ledkyb team to create the Ledkyb company website"
+					largeDesc="As part of the Ledkyb team I worked alongside other developers to create the 
+										company's website. My main responsibility for this project was to code a section
+										that displayed the Ledkyb team. Other responsibilities included reviewing pull
+										requests on GitHub from other developers, creating the functionality for the 
+										side tracker and communicating with the team to avoid conflicts. The project was
+										developed using React, GreenSock (GSAP), Bootstrap, Jenkins, MongoDB, Git and GitHub.
+										To view the Ledkyb website click on the image. "/>
+				<Card id="card2" img={fccImg} alt="free code camp riverside github" 
 					link="https://github.com/zootechdrum/Free_Code_Camp_Riverside"
 					smallDesc="An ongoing team project for the freeCodeCamp Riverside website. "
 					largeDesc="This website is being developed in hopes to connect new and aspiring programmers
 										within the Riverside and Inland Empire area. The website will provide new programmers with
 										resources on learning to program and a community to interact with. Working on this project has provided
-										me with an environment which requires team coordination and communication. My current
-										contributions to the website has been the navigation bar and the GitHub readme document.
+										me with an environment which requires team coordination and communication. 
 										To view the GitHub page for this website click on the image."/>
-				<Card id="card2" img={smilurImg} alt="free code camp riverside github" 
+				<Card id="card3" img={smilurImg} alt="free code camp riverside github" 
 					smallDesc="A social media Android app being developed by a small group."
 					largeDesc="Social media Android app that is currently under development. Currently I am
 										collaborating with one other developer and a designer to create a social media
@@ -37,7 +47,7 @@ const Portfolio = ({portfolioOpac}) => {
 										data from a database that uses PostgreSQL. Although my role consist mostly of 
 										back-end development, I occasionally work with my team on the front-end using Java 
 										and Android Studio."/>
-				<Card id="card3" img={simonImg} alt="simon game" 
+				<Card id="card4" img={simonImg} alt="simon game" 
 					link="https://codepen.io/Tonynguyen111997/full/wjowzW"
 					smallDesc="Simon game developed using HTML, CSS and Javascript. "
 					largeDesc="A Simon game developed for a freeCodeCamp.org project. The project
@@ -49,7 +59,7 @@ const Portfolio = ({portfolioOpac}) => {
 										button has its own distinct sound, which was provided by freeCodeCamp.
 										This project was developed before I learned about the ECMAScript 6 syntax.
 										To view the code, click on the link and press change view. "/>
-				<Card id="card4" img={tictactoeImg} alt="tic tac toe" 
+				<Card id="card5" img={tictactoeImg} alt="tic tac toe" 
 						link="https://codepen.io/Tonynguyen111997/full/QrbogG"
 						smallDesc="Tic Tac Toe game developed using HTML, CSS and Javascript."
 						largeDesc="A solo project developed as part of the freeCodeCamp.org curriculum.
@@ -58,7 +68,7 @@ const Portfolio = ({portfolioOpac}) => {
 											This game allows up to two players. The player will play against
 											a CPU if played alone. The game functions just like how a normal
 											tic tac toe game. This project was made using HTML, CSS and Javascript."/>
-				<Card id="card5" img={calculatorImg} alt="calculator app" 
+				<Card id="card6" img={calculatorImg} alt="calculator app" 
 						link="https://codepen.io/Tonynguyen111997/full/EEpQpB"
 						smallDesc="A calculator app on CodePen that was developed using HTML, CSS and Javascript."
 						largeDesc="Calculator app that functions just like a normal calculator. While developing
@@ -66,7 +76,7 @@ const Portfolio = ({portfolioOpac}) => {
 											 in which I could practice my critical thinking skills, because of the many edge
 											 cases that had to be covered. HTML, CSS and Javascript was used to create the 
 											 calculator. "/>
-				<Card id="card6" img={wikiViewerImg} alt="wiki viewer app" 
+				<Card id="card7" img={wikiViewerImg} alt="wiki viewer app" 
 						link="https://codepen.io/Tonynguyen111997/full/gvNWNa"
 						smallDesc="A simple app that searches Wikipedia for what the user inputs."
 						largeDesc="The Wikipedia app uses XMLHttpRequests to fetch JSON data from Wikipedia.
@@ -75,7 +85,7 @@ const Portfolio = ({portfolioOpac}) => {
 											 search and random button. The search button opens a search bar for the user to
 											 type and retrieve information from Wikipedia. The random button opens a new window
 											 that provides the user with random information found on Wikipedia."/>
-				<Card id="card7" img={weatherImg} alt="weather forecast app" 
+				<Card id="card8" img={weatherImg} alt="weather forecast app" 
 						link="https://codepen.io/Tonynguyen111997/full/gvqXeQ"
 						smallDesc="Weather app that checks user's local weather info using APIs and HTML5 Geolocation"
 						largeDesc="This app was intended as an exercise on using APIs and using geolocation.
