41993499b04e5481345189d249f2c80a9d434fd3
tonynguyen111997 <tonynguyen111997@gmail.com>
2019-04-20 22:42:47 -0700

    Update about-me section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -12,16 +12,17 @@ const AboutMe = ({aboutOpac}) => {
 		<div id="about-me-container" className="about-me">
 			<div className="about-me-container" style={{opacity: aboutOpac}}>
 				<h2>About Me</h2>
-				<img alt="self portrait" id="about" src={selfImg} />
+				<img alt="self portrait" id="about" src="https://avatars1.githubusercontent.com/u/36907562?s=460&v=4" />
 				<h3>Tony Nguyen</h3>
 				<h4>tonynguyen111997@gmail.com</h4>
 				<h3>Web Developer</h3>
-				<p>Self taught web developer that is always looking to grow and improve. 
-					I am a passionate learner that is eager to gain more experience. Learning 
-					to program has been a passion of mine for many years now. A key aspect 
-					of programming that has kept me interested in it is the problem solving. 
-					I enjoy problem solving, as it gives me a challenge and the opportunity to 
-					develop my critical thinking skills.</p>
+				<p>
+					Self taught web developer that is always looking to grow and improve. 
+					I am a passionate learner that is always eager to gain more experience. Learning 
+					to program has been a passion of mine for many years now, because there will always 
+					be something new to learn. I enjoy solving real life problems by developing technologies 
+					that solve them. 
+				</p>
 				<SocialNetworksButtons containerClass="about-me-btn-container"/>
 			</div>
 		</div>
