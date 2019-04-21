d56c20fbd360a347b4e71e09265b8f484056d8eb
tonynguyen111997 <tonynguyen111997@gmail.com>
2019-04-20 22:33:59 -0700

    update skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -17,12 +17,18 @@ const Skills = ({skillsOpac}) => {
 				<img alt="node.js logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNF2a5ZVtpCNlzLJgzk3biHx22qZVijBA8jO1Q40oUoDu1qNYl"/>
 				<img alt="bootstrap logo" src="https://devstickers.com/assets/img/cat/bootstrap.png" />
 				<img alt="sass logo" src="https://sass-lang.com/assets/img/styleguide/color-1c4aab2b.png"/>
-				<img alt="git logo" src="https://www.gitdvcs.com/img/logo/git-orange-light-backgroun-stacked.svg"/>
+				<img alt="git logo" src="https://git-scm.com/images/logos/downloads/Git-Icon-Black.png"/>
 				<img alt="GitHub logo" src="https://image.flaticon.com/icons/svg/25/25231.svg" />
 				<img alt="SQL icon" src="https://cdn0.iconfinder.com/data/icons/superuser-extension-light/512/675277-data_database_sql_query-512.png" />
 				<img alt="PostgreSQL logo" src="https://safenet.gemalto.com/uploadedImages/images/Logos/postgresql-logo.png" />
 				<img alt="MySQL logo" src="https://cdn.iconscout.com/icon/free/png-256/mysql-6-226028.png" />
 				<img alt="Knex logo" src="https://cdn.worldvectorlogo.com/logos/knex-1.svg" />
+				<img alt="TypeScript logo" src="https://raw.githubusercontent.com/remojansen/logo.ts/master/ts.png" />
+				<img alt="next logo" src="https://cdn.auth0.com/blog/logos/nextjs-logo.png" />
+				<img alt="jQuery logo" src="https://www.wpfaster.org/wp-content/uploads/2013/06/jquery-logo.png" />
+				<img alt="jest logo" src="https://ih1.redbubble.net/image.404023266.1965/pp,550x550.jpg" />
+				<img alt="linux logo" src="https://www.famouslogos.us/images/linux-logo.jpg" />
+				<img alt="adobe xd logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReAOMN3EwLJGE22vqSgm5ZeOhvQmsxYD8FPyLPfsgMx1HDfhRu" />
 			</div>
 		</div>
 	);
