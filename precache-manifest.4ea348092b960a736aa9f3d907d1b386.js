7a53c241f4c2ff2ad45aa2adf4dd7cddcb75b811
tonynguyen111997 <tonynguyen111997@gmail.com>
2019-06-29 02:17:08 -0700

    Updates

---
+++ precache-manifest.4ea348092b960a736aa9f3d907d1b386.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "af5a582a053f12556ed07876232bcc41",
+    "url": "/tonynguyen111997/old_portfolio/static/media/smilur.af5a582a.png"
+  },
+  {
+    "revision": "5e403b1d039b94a71d77",
+    "url": "/tonynguyen111997/old_portfolio/static/css/main.b3efa44e.chunk.css"
+  },
+  {
+    "revision": "01f9fba2de886d65488f",
+    "url": "/tonynguyen111997/old_portfolio/static/js/1.01f9fba2.chunk.js"
+  },
+  {
+    "revision": "153c84bac2a79133804a",
+    "url": "/tonynguyen111997/old_portfolio/static/js/runtime~main.153c84ba.js"
+  },
+  {
+    "revision": "e9d9240b085f27a73d25069ebf8396db",
+    "url": "/tonynguyen111997/old_portfolio/static/media/ledkyb.e9d9240b.png"
+  },
+  {
+    "revision": "faf57e9ebeb3735e792178ccbcb9637a",
+    "url": "/tonynguyen111997/old_portfolio/static/media/iesd_website.faf57e9e.png"
+  },
+  {
+    "revision": "4c1a6b4461b5629d860b538f13179121",
+    "url": "/tonynguyen111997/old_portfolio/static/media/fcc_riverside.4c1a6b44.png"
+  },
+  {
+    "revision": "5e403b1d039b94a71d77",
+    "url": "/tonynguyen111997/old_portfolio/static/js/main.5e403b1d.chunk.js"
+  },
+  {
+    "revision": "60c087af416a8d0dffd49dac8d53a70b",
+    "url": "/tonynguyen111997/old_portfolio/static/media/simon_game.60c087af.png"
+  },
+  {
+    "revision": "59d5f120fdfe4c928cc0be83c4581466",
+    "url": "/tonynguyen111997/old_portfolio/static/media/tic_tac_toe_game.59d5f120.png"
+  },
+  {
+    "revision": "9e6e0f2d38aff4086970012a21ddde49",
+    "url": "/tonynguyen111997/old_portfolio/static/media/calculator_app.9e6e0f2d.png"
+  },
+  {
+    "revision": "113b53f6191fa75920cb2494d3cf20ff",
+    "url": "/tonynguyen111997/old_portfolio/static/media/wiki_viewer.113b53f6.png"
+  },
+  {
+    "revision": "241d3621526e6d44348185da3669e44a",
+    "url": "/tonynguyen111997/old_portfolio/static/media/weather_forecast.241d3621.png"
+  },
+  {
+    "revision": "6045951fc1cc7166784cbcd01d0e34d8",
+    "url": "/tonynguyen111997/old_portfolio/index.html"
+  }
+];
